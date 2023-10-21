b6c12854a17e3cab3e1a43388da48a4c669a1ddc
sivakumar8838 <sivamss5882@gmail.com>
2023-10-21 13:28:22 +0530

    ADD

--- src/components/Card1Cy.jsx
+++ src/components/Card1Cy.jsx
@@ -1,21 +1,14 @@
 import React from 'react'
 
-export default function Card1() {
+export default function Card1Cy() {
   return (
-      <div><h1 className='text-center'>7 Best Full-Stack Development Online Courses [2023]
-      </h1>
-    
-         
-          <div class="elementor-widget-container">
-<img  src="https://www.guvi.in/blog/wp-content/uploads/2023/08/Feature-image-Best-Full-Stack-Development-Online-Courses.webp" className="m-4 elementor-animation-float attachment-full size-full wp-image-26726" alt="Feature image - Best Full-Stack Development Online Courses" loading="lazy" srcset="https://www.guvi.in/blog/wp-content/uploads/2023/08/Feature-image-Best-Full-Stack-Development-Online-Courses.webp 800w, https://www.guvi.in/blog/wp-content/uploads/2023/08/Feature-image-Best-Full-Stack-Development-Online-Courses-300x188.webp 300w, https://www.guvi.in/blog/wp-content/uploads/2023/08/Feature-image-Best-Full-Stack-Development-Online-Courses-768x480.webp 768w, https://www.guvi.in/blog/wp-content/uploads/2023/08/Feature-image-Best-Full-Stack-Development-Online-Courses-150x94.webp 150w" sizes="(max-width: 800px) 100vw, 800px"/> </div>
-          <p>
-          Today’s world is rapidly evolving into a technological landscape. Owing to these dynamics, the demand and scope for full-stack developers are constantly on the rise. Are you an aspiring web developer striving to enhance your web development skills and become a full-stack web developer?
+      <div><h1>What Is Hacking? Types of Hacking & More</h1>
+     <div className="elementor-widget-container">
+<img  src="https://www.guvi.in/blog/wp-content/uploads/2023/09/Feature-image-What-Is-Hacking_-Types-of-Hacking-More.webp" className="m-4 elementor-animation-float attachment-full size-full wp-image-26949" alt="Feature image -What Is Hacking_ Types of Hacking &amp; More" loading="lazy" srcset="https://www.guvi.in/blog/wp-content/uploads/2023/09/Feature-image-What-Is-Hacking_-Types-of-Hacking-More.webp 800w, https://www.guvi.in/blog/wp-content/uploads/2023/09/Feature-image-What-Is-Hacking_-Types-of-Hacking-More-300x188.webp 300w, https://www.guvi.in/blog/wp-content/uploads/2023/09/Feature-image-What-Is-Hacking_-Types-of-Hacking-More-768x480.webp 768w, https://www.guvi.in/blog/wp-content/uploads/2023/09/Feature-image-What-Is-Hacking_-Types-of-Hacking-More-150x94.webp 150w" sizes="(max-width: 800px) 100vw, 800px"/> </div>
+      <p>Have you ever wondered what hacking is all about? It’s a big deal in today’s tech-heavy world, and it can be both fascinating and scary. In this blog, we’re going to explore hacking in simple terms so that you can understand it better. We’ll look at the different kinds of hacking that exist. 
 
-If yes, you’ve come to the right place. In this comprehensive blog, you can find the list of the best full-stack development online courses available on the internet in 2023. Whether you’re a beginner or already have some programming experience, the online courses mentioned in this blog can provide you with the knowledge and skills needed to thrive as a full-stack developer.
+For example, there are hackers who try to steal your passwords and others who fight to protect your information. Some hackers even have strong beliefs and use their skills to support their causes. It’s a bit like a digital battlefield out there.
 
-So, look no further, choose the right course from the list below, and get started!
-          </p>
-      </div>
-
+By the end of this blog, you’ll have a better understanding of what hacking is all about, why it matters, and how you can protect yourself in the digital world. So, let’s begin this journey to unravel the mysteries of hacking in a simple and understandable way.</p></div>
   )
 }
