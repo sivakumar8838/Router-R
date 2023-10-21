62ccec0a2f4cb72410596306bd9279285afa218a
sivakumar8838 <sivamss5882@gmail.com>
2023-10-21 13:32:46 +0530

    add

--- src/components/Card2Cy.jsx
+++ src/components/Card2Cy.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Card1Cy() {
+export default function Card2Cy() {
   return (
       <div><h1>Cybersecurity Vs Ethical Hacking: Top 10 Differences</h1>
  <div className="elementor-widget-container">
